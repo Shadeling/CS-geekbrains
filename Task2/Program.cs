5913f648cb892a244251d5fdcf7cb4c7567d68f9
Shadeling <mironow.slav@ya.ru>
2021-12-08 12:48:55 +0300

    uploaded homework 1 to main

---
+++ Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+double wei, hei, IMT;
+Console.Write("Введите рост: ");
+string str = Console.ReadLine();
+wei = Convert.ToDouble(str);
+Console.Write("Введите вес: ");
+str = Console.ReadLine();
+hei = Convert.ToDouble(str);
+
+IMT = wei/(hei*hei);
+Console.Write("Индекс массы тела: {0}",IMT);
---
+++ Task4/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+double a=10, b=5;
+Console.Write($"a = {a}, b = {b}");
+a= b * a;
+b= a/b;
+a= a/b;
+Console.Write($"\na = {a}, b = {b}");
