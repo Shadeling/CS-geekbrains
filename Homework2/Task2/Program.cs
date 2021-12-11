2556286af017577aca0a5b5d484fb075ca2daedd
Shadeling <mironow.slav@ya.ru>
2021-12-11 22:24:09 +0300

    2nd homework 3 tasks commit

---
+++ Homework2/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать метод подсчета количества цифр числа.
+// Миронов Святослав
+int digit_num(int a){
+    int num = 0;
+    while(a>0){
+        num++;
+        a/=10;
+    }
+    return num
+}
+
+
+int a = 2134;
+Console.Write(digit_num(a));
